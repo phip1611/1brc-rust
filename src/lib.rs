a776897dab892dac2a00a3058bca992e06089fef
Philipp Schuster <phip1611@gmail.com>
2024-05-05 22:42:38 +0200

    back to much simpler single-threaded approach

--- src/lib.rs
+++ src/lib.rs
@@ -1,188 +1,103 @@
 use std::collections::BTreeMap;
 use std::fs::File;
-use std::io::Read;
-use std::ops::Deref;
+use std::io::{BufRead, BufReader};
 use std::path::Path;
 use std::str::FromStr;
-use std::sync::mpsc::Receiver;
-use std::thread::JoinHandle;
-use std::time::Instant;
 
 const READ_BUFFER_SIZE: usize = 0x10000000 /* 512 Mib */;
+const AVG_BYTES_PER_LINE: u64 = 15;
 
-#[derive(Debug, Default)]
-struct Stats(BTreeMap<String /* City */, Vec<f32> /* Measurements */>);
+#[derive(Copy, Clone, Debug)]
+struct AggregatedWeatherData {
+    min: f32,
+    max: f32,
+    sum: f32,
+    sample_count: usize,
+}
 
-impl Stats {
-    pub fn add_record(&mut self, city: &str, temp: &str) {
-        let temp = f32::from_str(temp).unwrap();
-
-        if self.0.contains_key(city) {
-            let measurements = self.0.get_mut(city).unwrap();
-            measurements.push(temp);
-        } else {
-            self.0.insert(city.to_string(), vec![temp]);
+impl Default for AggregatedWeatherData {
+    fn default() -> Self {
+        Self {
+            min: f32::MAX,
+            max: f32::MIN,
+            sum: 0.0,
+            sample_count: 0,
         }
-    }
-    pub fn merge_measurements(&mut self, city: &str, new_measurements: &[f32]) {
-        if self.0.contains_key(city) {
-            let measurements = self.0.get_mut(city).unwrap();
-            measurements.extend(new_measurements);
-        } else {
-            self.0.insert(city.to_string(), new_measurements.to_vec());
-        }
-    }
-
-    pub fn into_inner(self) -> BTreeMap<String /* City */, Vec<f32> /* Measurements */> {
-        self.0
     }
 }
 
-impl Deref for Stats {
-    type Target = BTreeMap<String, Vec<f32>>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
+impl AggregatedWeatherData {
+    #[inline]
+    fn add_datapoint(&mut self, measurement: f32) {
+        if measurement < self.min {
+            self.min = measurement
+        }
+        if measurement > self.max {
+            self.max = measurement
+        }
+        self.sum += measurement;
+        self.sample_count += 1;
     }
-}
 
-enum ThreadMessage {
-    Workload(Vec<u8>),
-    Stop,
+    fn avg(&self) -> f32 {
+        self.sum / self.sample_count as f32
+    }
 }
 
 /// Processes all data according to the 1brc challenge and prints the data
 /// to `<path>.processed.txt` in `{Abha=-23.0/18.0/59.2, Abidjan=-16.2/...`
 /// format, where the value of each key is <min>/<mean>/<max>.
-pub fn process_and_print(path: impl AsRef<Path>) {
-    let mut file = File::open(path).unwrap();
+///
+/// I didn't do specific "extreme" fine-tuning or testing of ideal buffer
+/// sizes and intermediate buffer sizes. This is a best-effort approach for a
+/// trade-off between readability, simplicity, and performance.
+pub fn process_and_print(path: impl AsRef<Path> + Clone) {
 
-    let mut thread_handles = Vec::with_capacity(64);
+    let file = File::open(path).unwrap();
+    let mut reader = BufReader::with_capacity(READ_BUFFER_SIZE, file);
 
-    // We will face situations where we don't entirely read the last line
-    // entirely in a read operation. So, these bytes are saved so they can be
-    // prepended to the new read data.
-    let mut remaining_bytes_from_previous_iter =
-        Vec::<u8>::with_capacity(32 /* refers to the line length  */);
+    let mut line_read_buf = String::with_capacity((AVG_BYTES_PER_LINE * 2) as usize);
 
-    // Main threads read data in a loop.
-    let begin = Instant::now();
+    let mut stats = BTreeMap::new();
 
-    loop {
-        // Allocations here are cheap compared to the intensive workloads
-        // in the other thread.
-        let mut read_buf = vec![0; READ_BUFFER_SIZE + remaining_bytes_from_previous_iter.len()];
-
-        // Prepend partial data that is left from previous iteration.
-        for (i, &byte) in remaining_bytes_from_previous_iter.iter().enumerate() {
-            read_buf[i] = byte;
-        }
-        // Read buf slice: don't override the just prepended bytes
-        let read_buf_slice = &mut read_buf[remaining_bytes_from_previous_iter.len()..];
-        remaining_bytes_from_previous_iter.clear();
-
-        // Note that .read() facilitates internal state which increases the
-        // internal file pointer.
-        let n = file.read(read_buf_slice).unwrap();
-
+    // let mut workload_lines_per_thread = vec![0_u8; estimated_capacity_per_thread_buf];
+    while let Ok(n) = reader.read_line(&mut line_read_buf) {
         if n == 0 {
             break;
         }
+        // remove trailing "newline"
+        let line = &line_read_buf[..line_read_buf.len() - 1];
 
-        // ensure vector shows true length
-        read_buf.truncate(n);
+        let (station, measurement) = line.split_once(';').unwrap();
+        let measurement = f32::from_str(measurement).unwrap();
 
-        let (real, remaining) = split_unfinished_line_from_slice(&read_buf);
-        remaining_bytes_from_previous_iter.clear();
-        remaining_bytes_from_previous_iter.extend(remaining);
+        let weather_data = stats
+            .entry(station.to_string())
+            .or_insert_with(|| AggregatedWeatherData::default());
+        weather_data.add_datapoint(measurement);
 
-        // ensure vector shows true length
-        read_buf.truncate(real.len());
-
-        let (sender, receiver) = std::sync::mpsc::channel::<ThreadMessage>();
-        let handle = spawn_worker_thread(receiver);
-        thread_handles.push(handle);
-        // These allocations won't happen too often. Relatively cheap.
-        sender.send(ThreadMessage::Workload(read_buf)).unwrap();
-        sender.send(ThreadMessage::Stop).unwrap();
+        line_read_buf.clear();
     }
-    assert!(
-        remaining_bytes_from_previous_iter.is_empty(),
-        "measurements.txt must end with newline"
-    );
 
-    eprintln!("time in IO thread: {:?}", begin.elapsed());
+    print_results(stats.into_iter());
+}
 
-    // Combine all stats from all threads.
-    let stats = thread_handles
-        .into_iter()
-        .map(|h| h.join().unwrap())
-        .reduce(|mut l, r| {
-            r.iter()
-                .for_each(|(city, measurements)| l.merge_measurements(&city, measurements));
-            l
-        })
-        .unwrap();
-
+fn print_results(stats: impl Iterator<Item = (String, AggregatedWeatherData)> + ExactSizeIterator) {
     print!("{{");
+    let n = stats.len();
     stats
-        .into_inner()
-        .into_iter()
-        .for_each(|(city, mut measurements)| {
-            measurements.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap());
-            let sum = measurements.iter().fold(0.0, |acc, next| acc + next);
-            let avg = sum / measurements.len() as f32;
-            let min = measurements[0];
-            let max = measurements[measurements.len() - 1];
-            print!("{city}={min:.1}/{avg:.1}/{max:.1}, ")
+        .enumerate()
+        .map(|(index, x)| (index == n - 1, x))
+        .for_each(|(is_last, (city, measurements))| {
+            print!(
+                "{city}={:.1}/{:.1}/{:.1}",
+                measurements.min,
+                measurements.avg(),
+                measurements.max
+            );
+            if !is_last {
+                print!(", ");
+            }
         });
     println!("}}");
-}
-
-fn spawn_worker_thread(receiver: Receiver<ThreadMessage>) -> JoinHandle<Stats> {
-    std::thread::spawn(move || {
-        let begin = Instant::now();
-        let mut stats = Stats::default();
-
-        while let ThreadMessage::Workload(vec) = receiver.recv().unwrap() {
-            let data = unsafe { core::str::from_utf8_unchecked(&vec) };
-            data.lines().for_each(|line| {
-                let (city, measurement) = line.split_once(';').unwrap();
-                stats.add_record(city, measurement)
-            });
-        }
-
-        eprintln!("time in calculation thread: {:?}", begin.elapsed());
-
-        stats
-    })
-}
-
-fn split_unfinished_line_from_slice(
-    bytes: &[u8],
-) -> (
-    &[u8], /* only full lines */
-    &[u8], /* remaining begin line */
-) {
-    let position_of_last_newline = bytes.iter().rev().position(|&byte| byte == b'\n').unwrap();
-    let position_of_last_newline = bytes.len() - 1 - position_of_last_newline;
-    // includes the newline as last character
-    let slice_with_full_lines = &bytes[0..=position_of_last_newline];
-    // Doesn't include the newline
-    let remaining_bytes = &bytes[position_of_last_newline + 1..];
-
-    (slice_with_full_lines, remaining_bytes)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_split_unfinished_line_from_slice() {
-        let input = "foobar\nremaining";
-        let (full, remaining) = split_unfinished_line_from_slice(input.as_bytes());
-        assert_eq!("foobar\n".as_bytes(), full);
-        assert_eq!("remaining".as_bytes(), remaining);
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use std::time::Instant;
 
 fn main() {
     let begin = Instant::now();
-    let file = std::env::args().skip(1).next().unwrap_or("./measurements.txt".to_string());
+    let file = std::env::args()
+        .nth(1)
+        .unwrap_or("./measurements.txt".to_string());
     phips_1brc::process_and_print(file);
     println!("took {:?}", begin.elapsed());
 }
