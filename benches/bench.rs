1a698810f9feb5d7b89dce8412285c98eb750c27
Philipp Schuster <phip1611@gmail.com>
2024-05-06 19:58:09 +0200

    bench: add multithreaded bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,17 +1,34 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("1brc (100 entries)", |b| {
+fn single_threaded_benchmarks(c: &mut Criterion) {
+    c.bench_function("single: 1brc (100 entries)", |b| {
         b.iter(|| {
             phips_1brc::process_single_threaded("./measurements_100.txt", false);
         })
     });
-    c.bench_function("1brc (1000000 entries )", |b| {
+    c.bench_function("single: 1brc (1000000 entries )", |b| {
         b.iter(|| {
             phips_1brc::process_single_threaded("./measurements_1000000.txt", false);
         })
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn multi_threaded_benchmarks(c: &mut Criterion) {
+    c.bench_function("multi: 1brc (100 entries)", |b| {
+        b.iter(|| {
+            phips_1brc::process_multi_threaded("./measurements_100.txt", false);
+        })
+    });
+    c.bench_function("multi: 1brc (1000000 entries )", |b| {
+        b.iter(|| {
+            phips_1brc::process_multi_threaded("./measurements_1000000.txt", false);
+        })
+    });
+}
+
+criterion_group!(
+    benches,
+    single_threaded_benchmarks,
+    multi_threaded_benchmarks
+);
 criterion_main!(benches);
