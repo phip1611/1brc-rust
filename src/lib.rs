9e29c7b468459e2a95feee9191693372f74be44a
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2024-05-06 12:34:19 +0200

    xxx multithreading
    I have to check if the resutls are still the same

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
+use std::cmp::{max_by, min, min_by};
 use fnv::FnvHashMap as HashMap;
 use memmap::MmapOptions;
 use std::fs::File;
 use std::path::Path;
 use std::str::FromStr;
+use std::thread::JoinHandle;
 
 const CITIES_IN_DATASET: usize = 416;
 
@@ -60,44 +62,60 @@ pub fn process(
     let file_bytes: &'static [u8] =
         unsafe { core::slice::from_raw_parts(mmap.as_ptr(), mmap.len()) };
 
-    let mut stats = HashMap::with_capacity_and_hasher(CITIES_IN_DATASET, Default::default());
+
+    let thread_count: usize = std::thread::available_parallelism().unwrap().into();
+    let bytes_per_thread = file_bytes.len().div_ceil(thread_count);
+
+    let mut thread_handles = vec![];
 
     // In each iteration, I read a line in two dedicated steps:
     // 1.) read city name
     // 2.) read value
     let mut consumed_bytes_count = 0;
+
+    // Spawn worker threads with the correct data set for each.
     while consumed_bytes_count < file_bytes.len() {
         // Remaining bytes for this loop iteration.
         let remaining_bytes = &file_bytes[consumed_bytes_count..];
 
-        let n1 = memchr::memchr(b';', &remaining_bytes).unwrap();
-        let station = &remaining_bytes[0..n1];
-        let station = unsafe { core::str::from_utf8_unchecked(station) };
+        // The end of the block where the search for the actual end (newline)
+        // begins.
+        let block_end_begin_search_index = min(bytes_per_thread, remaining_bytes.len());
 
-        // +1: skip "\n"
-        let search_begin_i = n1 + 1;
-        let n2 = memchr::memchr(b'\n', &remaining_bytes[search_begin_i..])
-            .map(|pos| pos + search_begin_i)
+        // Prepare a data block for the next thread; ensures it ends with a
+        // newline
+        let data_for_thread_end_index = remaining_bytes.iter()
+            .skip(block_end_begin_search_index - 1)
+            .position(|&byte| byte == b'\n')
+            .map(|index| index + block_end_begin_search_index)
             .unwrap();
+        let data_for_thread = &remaining_bytes[0..data_for_thread_end_index];
+        consumed_bytes_count += data_for_thread_end_index + 1;
 
-        // +1: skip ";'
-        let measurement = &remaining_bytes[(n1 + 1)..n2];
-        let measurement = unsafe { core::str::from_utf8_unchecked(measurement) };
-        let measurement = f32::from_str(measurement).unwrap();
-
-        // +1: skip "\n"
-        consumed_bytes_count += n2 + 1;
-
-        // In the data set, there aren't that many different entries.
-        stats
-            .entry(station)
-            .and_modify(|data: &mut AggregatedWeatherData| data.add_datapoint(measurement))
-            .or_insert_with(|| {
-                let mut data = AggregatedWeatherData::default();
-                data.add_datapoint(measurement);
-                data
-            });
+        let handle = spawn_worker_thread(data_for_thread);
+        thread_handles.push(handle);
     }
+
+    // Ensure that we really created the right amount of threads.
+    debug_assert_eq!(thread_handles.len(), thread_count);
+
+    // Aggregate all thread results in the main thread. Combine them in a map.
+    // This takes ~300µs on my machine with 16 cores. It is not worth it to
+    // distribute that work onto threads in a (reverse-)pyramid-like structure.
+    let stats = thread_handles.into_iter().map(|h| h.join().unwrap())
+        .reduce(|mut acc, next| {
+            next.into_iter().for_each(|(station, new_data)| {
+                acc.entry(station)
+                    .and_modify(|data| {
+                        data.max = max_by(data.max, new_data.max, |a, b| a.partial_cmp(b).unwrap());
+                        data.min = min_by(data.min, new_data.min, |a, b| a.partial_cmp(b).unwrap());
+                        data.sum += new_data.sum;
+                        data.sample_count += new_data.sample_count;
+                    })
+                    .or_insert(new_data);
+            });
+            acc
+        }).unwrap();
 
     // sort in a vec: quicker than in a btreemap
     let mut stats = stats.into_iter().collect::<Vec<_>>();
@@ -107,7 +125,49 @@ pub fn process(
     (mmap, stats)
 }
 
-pub fn print_results<'a>(stats: impl ExactSizeIterator<Item = (&'a str, AggregatedWeatherData)>) {
+/// Spawns a worker thread. Each thread operates on its dedicated slice of the
+/// file. Each chunk per thread is guaranteed to end with a newline.
+fn spawn_worker_thread(bytes: &'static [u8]) -> JoinHandle<HashMap<&'static str, AggregatedWeatherData>> {
+    let mut consumed_bytes_count = 0;
+    std::thread::spawn(move || {
+        let mut stats = HashMap::with_capacity_and_hasher(CITIES_IN_DATASET, Default::default());
+
+        while consumed_bytes_count < bytes.len() {
+            let remaining_bytes = &bytes[consumed_bytes_count..];
+
+            let n1 = memchr::memchr(b';', &remaining_bytes).unwrap();
+            let station = &remaining_bytes[0..n1];
+            let station = unsafe { core::str::from_utf8_unchecked(station) };
+
+            // +1: skip "\n"
+            let search_begin_i = n1 + 1;
+            let n2 = memchr::memchr(b'\n', &remaining_bytes[search_begin_i..])
+                .map(|pos| pos + search_begin_i)
+                .unwrap();
+
+            // +1: skip ";'
+            let measurement = &remaining_bytes[(n1 + 1)..n2];
+            let measurement = unsafe { core::str::from_utf8_unchecked(measurement) };
+            let measurement = f32::from_str(measurement).unwrap();
+
+            // +1: skip "\n"
+            consumed_bytes_count += n2 + 1;
+
+            // In the data set, there aren't that many different entries.
+            stats
+                .entry(station)
+                .and_modify(|data: &mut AggregatedWeatherData| data.add_datapoint(measurement))
+                .or_insert_with(|| {
+                    let mut data = AggregatedWeatherData::default();
+                    data.add_datapoint(measurement);
+                    data
+                });
+        }
+        stats
+    })
+}
+
+pub fn print_results<'a>(stats: impl ExactSizeIterator<Item=(&'a str, AggregatedWeatherData)>) {
     print!("{{");
     let n = stats.len();
     stats
