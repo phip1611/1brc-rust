661acf8fd0607ee1fbc9a34789b0971d32c44e75
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2024-05-06 11:42:10 +0200

    performance: further optimized by 25%
    Less buffering, less copying. Just work on the mmap'ed memory

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,10 @@
 use fnv::FnvHashMap as HashMap;
-use likely_stable::{likely, unlikely};
+use likely_stable::likely;
 use memmap::MmapOptions;
 use std::fs::File;
-use std::io::{BufRead, Cursor};
 use std::path::Path;
 use std::str::FromStr;
 
-/// Obtained from `wc -L ./measurements.txt`
-const MAX_LINE_LEN: usize = 32 + 1 /* newline */;
 const CITIES_IN_DATASET: usize = 416;
 
 #[derive(Copy, Clone, Debug)]
@@ -59,30 +56,31 @@ pub fn process(path: impl AsRef<Path> + Clone) -> Vec<(String, AggregatedWeather
     let file = File::open(path).unwrap();
     let mmap = unsafe { MmapOptions::new().map(&file).unwrap() };
     let file_bytes = unsafe { core::slice::from_raw_parts(mmap.as_ptr(), mmap.len()) };
-    let mut reader = Cursor::new(file_bytes);
 
-    let mut line_read_buf = Vec::with_capacity(MAX_LINE_LEN);
     let mut stats = HashMap::with_capacity_and_hasher(CITIES_IN_DATASET, Default::default());
 
-    while let Ok(n1) = reader.read_until(b';', &mut line_read_buf) {
-        if unlikely(n1 == 0) {
-            break;
-        }
-        let n2 = reader.read_until(b'\n', &mut line_read_buf).unwrap();
-        debug_assert!(
-            n2 > 0,
-            "Malformed data. Data must always have a newline after a ;"
-        );
+    let mut consumed_bytes_count = 0;
+    while consumed_bytes_count < file_bytes.len() {
+        let remaining_bytes = &file_bytes[consumed_bytes_count..];
+        // TODO use memchr?
+        let n1 = remaining_bytes
+            .iter()
+            .position(|&byte| byte == b';')
+            .unwrap();
+        let station = &remaining_bytes[0..n1];
+        let station = unsafe { core::str::from_utf8_unchecked(station) };
 
-        let station = unsafe { core::str::from_utf8_unchecked(&line_read_buf[0..n1]) };
-        // remove trailing ';'
-        let station = &station[0..station.len() - 1];
-
-        let measurement = unsafe { core::str::from_utf8_unchecked(&line_read_buf[n1..]) };
-        // remove trailing '\n'
-        let measurement = &measurement[0..measurement.len() - 1];
-
+        let n2 = remaining_bytes
+            .iter()
+            .position(|&byte| byte == b'\n')
+            .unwrap();
+        // +1: skip ";'
+        let measurement = &remaining_bytes[(n1 + 1)..n2];
+        let measurement = unsafe { core::str::from_utf8_unchecked(measurement) };
         let measurement = f32::from_str(measurement).unwrap();
+
+        // +1: skip "\n"
+        consumed_bytes_count += n2 + 1;
 
         // In the data set, there aren't that many different entries.
         let data = stats.get_mut(station);
@@ -94,9 +92,6 @@ pub fn process(path: impl AsRef<Path> + Clone) -> Vec<(String, AggregatedWeather
             data.add_datapoint(measurement);
             stats.insert(station.to_string(), data);
         }
-
-        // Clear for next iteration.
-        line_read_buf.clear();
     }
 
     // sort in a vec: quicker than in a btreemap
